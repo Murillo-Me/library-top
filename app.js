9dd148c0f4b3011bdaf92b7e11a7e81279848caa
Murillo Mendes <murillomendes95@hotmail.com>
2021-09-25 20:39:37 -0300

    Added main functionalities

--- app.js
+++ app.js
@@ -20,24 +20,103 @@ Book.prototype.addBookToLibrary = addBookToLibrary
 invisibleMan.addBookToLibrary()
 beautifulWorld.addBookToLibrary()
 briefHistoryOfTime.addBookToLibrary()
+invisibleMan.addBookToLibrary()
+
 
 function displayLibrary(library) {
-    const bookRowDiv = document.querySelector('.book-row')
-    library.forEach(book => {
-        const bookDiv = document.createElement('div')
-        bookDiv.setAttribute('class', 'book-container')
-        bookDiv.innerHTML = `
-        <img src="./images/cover-example.jpg">
-        <h3 class="book-title">${book.title}</h3>
-        <span class="book-author">${book.author}</span>
-        <p class="book-pages">${book.numberOfPages} pages</p>
-        `
-        bookRowDiv.append(bookDiv)
+    const bookshelfDiv = document.querySelector('.bookshelf-main-container')
+    bookshelfDiv.textContent = ''
 
+    if (library.length > 0) {
+        library.forEach((book, i) => {
+            if (i == 0) {
+                const rowBottom = document.createElement('div')
+                rowBottom.classList.add('row-bottom')
+                bookshelfDiv.append(rowBottom)
+            }
+
+            const bookDiv = document.createElement('div')
+            bookDiv.setAttribute('class', 'book-container')
+            bookDiv.setAttribute('data-key', `${i}`)
+            bookDiv.innerHTML = `
+            <button class="closeBtn deleteBookBtn" data-key="${i}">X</button>
+            <img src="./images/cover-example.jpg">
+            <h3 class="book-title">${book.title}</h3>
+            <span class="book-author">${book.author}</span>
+            <p class="book-pages">${book.numberOfPages} pages</p>
+            `
+            
+            bookshelfDiv.append(bookDiv)
+
+            if ((i+1) % 4 == 0 && i != 0 && (i+1) != library.length) {
+                const rowBottom = document.createElement('div')
+                rowBottom.classList.add('row-bottom')
+                bookshelfDiv.append(rowBottom)
+            }
+        })
+    } else {
+        bookshelfDiv.style.height = '283px'
+
+        const rowBottom = document.createElement('div')
+        rowBottom.classList.add('row-bottom')
+        bookshelfDiv.append(rowBottom)
+    }
+
+    const rowBottom = document.createElement('div')
+    rowBottom.classList.add('row-bottom')
+    bookshelfDiv.append(rowBottom)
+
+    let allDeleteBookBtn = document.querySelectorAll('.deleteBookBtn')
+    allDeleteBookBtn.forEach(btn => btn.addEventListener('click', deleteBook))
+}
+
+function updateIndexes() {
+    const books = document.querySelectorAll('.book-container')
+    books.forEach((book, i) => {
+        book.setAttribute('data-key', i)
+        book.querySelector('.deleteBookBtn').setAttribute('data-key', i)
     })
+}
+
+function deleteBook() {
+    console.log(this);
+    const bookIndex = this.getAttribute('data-key')
+    const divToRemove = document.querySelector(`div[data-key="${bookIndex}"]`)
+    divToRemove.remove()
+    myLibrary.splice(bookIndex,1)
+    updateIndexes()
+    displayLibrary(myLibrary)
 }
 
 displayLibrary(myLibrary);
 
-const btnSubmitNewBook = document.getElementById('submitBook')
-console.log(btnSubmitNewBook);
+const addBookForm = document.getElementById('addBookForm')
+addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    bookData = new FormData(addBookForm)
+    const bookTitle = bookData.get('book--title')
+    const bookAuthor = bookData.get('book--author')
+    const bookPages = bookData.get('book--pages')
+    
+    const newBook = new Book(bookTitle, bookAuthor, bookPages, false)
+    newBook.addBookToLibrary()
+    displayLibrary(myLibrary)
+})
+
+const closeBtn = document.getElementById('closeBtn')
+closeBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    addBookForm.classList.toggle('active')
+})
+
+// (e) => {
+    
+//     const bookIndex = e.currentTarget.getAttribute('data-key')
+//     const divToRemove = document.querySelector(`div[data-key="${bookIndex}"]`)
+//     divToRemove.remove()
+//     myLibrary.splice(bookIndex,1)
+//     updateIndexes()
+//     console.log(allDeleteBookBtn);
+//     displayLibrary(myLibrary)
+//     allDeleteBookBtn = document.querySelectorAll('.deleteBookBtn')
+// }))
