2911a132be3c21e93e86e8dbbfb87fa71c14ee9b
Murillo Mendes <murillomendes95@hotmail.com>
2021-09-26 01:19:07 -0300

    Added responsive design and multiple functionalities. MVP.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 function Book(title, author, numberOfPages, isRead) {
     this.title = title;
     this.author = author;
@@ -7,21 +5,9 @@ function Book(title, author, numberOfPages, isRead) {
     this.isRead = isRead;
 }
 
-const invisibleMan = new Book('Invisible Man', 'Ralph Ellison', 581, false)
-const beautifulWorld = new Book('Beautiful World, Where Are You: A Novel', 'Sally Rooney', 353, false)
-const briefHistoryOfTime = new Book('A Brief History of Time', 'Stephen Hawking', 212, true)
-
 function addBookToLibrary() {
     myLibrary.push(this)
 }
-
-Book.prototype.addBookToLibrary = addBookToLibrary
-
-invisibleMan.addBookToLibrary()
-beautifulWorld.addBookToLibrary()
-briefHistoryOfTime.addBookToLibrary()
-invisibleMan.addBookToLibrary()
-
 
 function displayLibrary(library) {
     const bookshelfDiv = document.querySelector('.bookshelf-main-container')
@@ -40,9 +26,10 @@ function displayLibrary(library) {
             bookDiv.setAttribute('data-key', `${i}`)
             bookDiv.innerHTML = `
             <button class="closeBtn deleteBookBtn" data-key="${i}">X</button>
-            <img src="./images/cover-example.jpg">
+            <img src="./images/cover-example-placeholder.jpg">
             <h3 class="book-title">${book.title}</h3>
             <span class="book-author">${book.author}</span>
+            <button class="book-status-btn ${(book.isRead) ? 'read' : ''}" data-key="${i}">${(book.isRead) ? 'Already read' : 'Not read'}</button>
             <p class="book-pages">${book.numberOfPages} pages</p>
             `
             
@@ -68,6 +55,9 @@ function displayLibrary(library) {
 
     let allDeleteBookBtn = document.querySelectorAll('.deleteBookBtn')
     allDeleteBookBtn.forEach(btn => btn.addEventListener('click', deleteBook))
+
+    const readBtn = document.querySelectorAll('.book-status-btn')
+    readBtn.forEach(btn => btn.addEventListener('click', changeReadStatus))
 }
 
 function updateIndexes() {
@@ -88,6 +78,43 @@ function deleteBook() {
     displayLibrary(myLibrary)
 }
 
+function changeReadStatus() {
+    const bookIndex = this.getAttribute('data-key')
+    this.classList.toggle('read')
+    if (this.classList.contains('read')) {
+        this.textContent = 'Already read'
+        myLibrary[bookIndex].isRead = true
+    } else {
+        this.textContent = 'Not read'
+        myLibrary[bookIndex].isRead = false
+    }
+}
+
+function clearAllBooks() {
+    myLibrary.length = 0
+    displayLibrary()
+}
+
+let myLibrary = [];
+
+const invisibleMan = new Book('Invisible Man', 'Ralph Ellison', 581, true)
+const beautifulWorld = new Book('Beautiful World, Where Are You: A Novel', 'Sally Rooney', 353, false)
+const briefHistoryOfTime = new Book('A Brief History of Time', 'Stephen Hawking', 212, true)
+const dontCloseYourEyes = new Book("Don't Close Your Eyes", 'Lynessa Layne', 326, false)
+const theGreatGatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 200, false)
+const thePurse = new Book('The Purse', 'Julie A. Burns', 278, false)
+const timeAndTimeAgain = new Book('Time and Time Again', 'Ben Elton', 386, true)
+
+Book.prototype.addBookToLibrary = addBookToLibrary
+
+invisibleMan.addBookToLibrary()
+beautifulWorld.addBookToLibrary()
+briefHistoryOfTime.addBookToLibrary()
+dontCloseYourEyes.addBookToLibrary()
+theGreatGatsby.addBookToLibrary()
+thePurse.addBookToLibrary()
+timeAndTimeAgain.addBookToLibrary()
+
 displayLibrary(myLibrary);
 
 const addBookForm = document.getElementById('addBookForm')
@@ -97,26 +124,24 @@ addBookForm.addEventListener('submit', (e) => {
     const bookTitle = bookData.get('book--title')
     const bookAuthor = bookData.get('book--author')
     const bookPages = bookData.get('book--pages')
+    const bookIsRead = bookData.get('read-status-checkbox')
     
-    const newBook = new Book(bookTitle, bookAuthor, bookPages, false)
+    const newBook = new Book(bookTitle, bookAuthor, bookPages, bookIsRead)
     newBook.addBookToLibrary()
     displayLibrary(myLibrary)
+    addBookForm.classList.toggle('active')
 })
+
+const newBookBtn = document.getElementById('new-book')
+newBookBtn.addEventListener('click', () => {
+    addBookForm.classList.toggle('active')
+})
+
+const clearAllBtn = document.getElementById('clear-all-btn')
+clearAllBtn.addEventListener('click', clearAllBooks)
 
 const closeBtn = document.getElementById('closeBtn')
 closeBtn.addEventListener('click', (e) => {
     e.preventDefault()
     addBookForm.classList.toggle('active')
 })
-
-// (e) => {
-    
-//     const bookIndex = e.currentTarget.getAttribute('data-key')
-//     const divToRemove = document.querySelector(`div[data-key="${bookIndex}"]`)
-//     divToRemove.remove()
-//     myLibrary.splice(bookIndex,1)
-//     updateIndexes()
-//     console.log(allDeleteBookBtn);
-//     displayLibrary(myLibrary)
-//     allDeleteBookBtn = document.querySelectorAll('.deleteBookBtn')
-// }))
